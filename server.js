584e1324d0438d8e82db30c5ef3741dba4128af2
mattschneble <136631410+mattschneble@users.noreply.github.com>
2023-07-27 15:25:59 -0700

    added pseudocode and installed missing npm

---
+++ server.js
@@ -0,0 +1,16 @@
+// import all packages
+// refer port
+
+// boiler plate info
+// set route to db.json file
+    // use GET /api/notes
+// route for index.html
+    // use GET * 
+// route for notes.html 
+    // use GET /notes
+// route for POST = when the user inputs information for new note
+    //POST /api/notes
+// OPTIONAL: route for delete (remove using the id property), rewrite to the 'db.json' file
+    //DELETE /api/nodes/:id
+
+// listen on port
