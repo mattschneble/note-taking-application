ee254f8376073d630e97d29e7da3466d2c4ae208
mattschneble <136631410+mattschneble@users.noreply.github.com>
2023-07-31 16:17:53 -0700

    updated server.js with remaining calls

--- server.js
+++ server.js
@@ -14,28 +14,57 @@ app.use(express.json());
 // set up express app to handle static files and set root directory as the public folder
 app.use(express.static('public'));
 
-// set route to db.json file
+// set route to index.html
+// route for GET = when the user clicks on the 'Get Started' button
 app.get('/api/notes', (req, res) => {
     // read the db.json file
     fs.readFile('./db/db.json', (err, data) => {
         // throw error if there is one
-        if (err) throw err;
+        if (err) throw err
+        else {
         // parse the data
         const notes = JSON.parse(data);
         // send the parsed data back to the client
         res.json(notes);
-    });
+        }
+    })
+});
+ 
+// route for notes.html 
+// route for GET = when the user clicks on the 'Get Started' button
+app.get('/notes', (req, res) => {
+    // send the notes.html file to the client
+    res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
+// route for POST = when the user inputs information for new note and clicks on the save button
+app.post('/api/notes', (req, res) => {
+    // retrieve the new note from the request body
+    const originalNote = req.body;
+    // read the db.json file
+    fs.readFile('./db/db.json', (err, data) => {
+        // throw error if there is one
+        if (err) throw err
+        else {
+        // parse the data
+        const notesData = JSON.parse(data);
+        // add the new note to the array
+        notesData.push(req.body);
+        // stringify the data
+        const stringifiedData = JSON.stringify(notesData);
+        // write the data to the db.json file
+        fs.writeFile('./db/db.json', stringifiedData, (err) => {
+            // throw error if there is one
+            if (err) throw err
+            else {
+            // send the parsed data back to the client
+            res.json(notesData);
+            }
+        })
+        }
+    })
+});
 
-// route for index.html
-    // use GET * 
-// route for notes.html 
-    // use GET /notes
-// route for POST = when the user inputs information for new note
-    // use POST /api/notes
-// OPTIONAL: route for delete (remove using the id property), rewrite to the 'db.json' file
-    // use DELETE /api/nodes/:id
 
 // listen on port
 app.listen(PORT, () => {
